78017c0f0863397ca91ab8c32aadf861630d2c1f
unknown <cortega.code@gmail.com>
2021-06-01 15:33:52 -0400

    initial commit

---
+++ Src/Emulation/Cpu.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Emulation
+{
+    public class Cpu
+    {
+    }
+}
---
+++ Src/Emulator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Emulation;
+
+namespace Emulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cpu = new Cpu();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
